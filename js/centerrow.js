aff8d47d9a732f3ea83649a5f95897bb484e6c3c
Kim Nguyen <design@kimisgold.com>
2017-03-16 15:41:14 -0400

    Justify exhibit nav left on mobile, refine search on mobile.

--- js/centerrow.js
+++ js/centerrow.js
@@ -1,5 +1,7 @@
 (function($) {
     $(document).ready(function() {
+        $('#advanced-form').parents('#search-container').addClass('with-advanced');
+
         $('.search-toggle').click(function() {
             $('#search-form').toggleClass('closed').toggleClass('open');
             if ($('#search-form').hasClass('open')) {
