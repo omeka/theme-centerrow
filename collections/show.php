fe7dbbbba3a8510e7416760ae3ffa640a6b24cb6
Kim Nguyen <design@kimisgold.com>
2018-02-13 17:06:43 -0500

    Update and style view items link.
    (omeka/Omeka#827)

--- collections/show.php
+++ collections/show.php
@@ -1,5 +1,6 @@
 <?php
 $collectionTitle = metadata('collection', 'display_title');
+$totalItems = metadata('collection', 'total_items');
 ?>
 
 <?php echo head(array('title' => $collectionTitle, 'bodyclass' => 'collections show')); ?>
@@ -9,8 +10,8 @@ $collectionTitle = metadata('collection', 'display_title');
 <?php echo all_element_texts('collection'); ?>
 
 <div id="collection-items">
-    <h2><?php echo link_to_items_browse(__('Items in the %s Collection', $collectionTitle), array('collection' => metadata('collection', 'id'))); ?></h2>
-    <?php if (metadata('collection', 'total_items') > 0): ?>
+    <h2><?php echo __('Collection Items'); ?></h2>
+    <?php if ($totalItems > 0): ?>
         <?php foreach (loop('items') as $item): ?>
         <?php $itemTitle = metadata('item', 'display_title'); ?>
         <div class="item hentry">
@@ -35,6 +36,7 @@ $collectionTitle = metadata('collection', 'display_title');
             </div><!-- end class="item-meta" -->
         </div><!-- end class="item hentry" -->
         <?php endforeach; ?>
+        <?php echo link_to_items_browse(__(plural('View item', 'View all %s items', $totalItems), $totalItems), array('collection' => metadata('collection', 'id')), array('class' => 'view-items-link')); ?>
     <?php else: ?>
         <p><?php echo __("There are currently no items within this collection."); ?></p>
     <?php endif; ?>
