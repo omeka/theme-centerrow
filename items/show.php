cabbf12f4d58f7c712c488a4f173a82d99afaa37
Kim Nguyen <design@kimisgold.com>
2019-03-13 14:56:48 -0400

    Replace 'Other Media' with 'Files' heading.

--- items/show.php
+++ items/show.php
@@ -57,7 +57,7 @@ $hasImages = (count($images) > 0);
 
 <?php if ((count($nonImages) > 0) && get_theme_option('other_media') == 1): ?>
 <div id="other-media" class="element">
-    <h3>Other Media</h3>
+    <h3><?php echo __('Files'); ?></h3>
     <?php foreach ($nonImages as $nonImage): ?>
     <div class="element-text"><a href="<?php echo file_display_url($nonImage, 'original'); ?>"><?php echo metadata($nonImage, 'display_title'); ?> - <?php echo $nonImage->mime_type; ?></a></div>
     <?php endforeach; ?>
