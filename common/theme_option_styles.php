6356aca5de3b918ed7cb33a71f5c61ebf2eb378b
Kim Nguyen <design@kimisgold.com>
2021-11-23 15:56:33 -0500

    Add new banner options, adjust open subnav widths.

--- common/theme_option_styles.php
+++ common/theme_option_styles.php
@@ -2,6 +2,11 @@
     ($bodyBgColor = get_theme_option('body_bg_color')) || ($buttonBgColor = "#FFFFFF");
     ($borderColor = get_theme_option('border_color')) || ($buttonBgColor = "#DEDEDE");
     ($linkColor = get_theme_option('link_color')) || ($linkColor = "#C72E2E");
+    ($headerImageHeight = get_theme_option('header_image_height')) || ($headerImageHeight = "auto");
+    $headerImageHeightMobile = get_theme_option('header_image_height_mobile');
+    $headerEverywhere = get_theme_option('header_everywhere');
+    $headerImagePosition = get_theme_option('header_image_position');
+    $headerImagePosition = str_replace('_', '-', $headerImagePosition);
 ?>
 
 <style>
@@ -41,4 +46,22 @@ a,
 #exhibit-pages .current a {
     color: <?php echo $linkColor; ?>;
 }
+
+<?php if ($headerEverywhere !== '1'): ?>
+#home #header-image {
+    display: none;
+}
+<?php endif; ?>
+
+#header-image {
+    height: <?php echo $headerImageHeight; ?>;
+    align-items: <?php echo $headerImagePosition; ?>;
+}
+<?php if ($headerImageHeightMobile !== ''): ?>
+@media screen and (max-width:640px) {
+    #header-image {
+        height: <?php echo $headerImageHeightMobile; ?>;
+    }
+}
+<?php endif; ?>
 </style>
